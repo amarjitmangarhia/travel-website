dbfe17eb6c232f468ced733d8a4793eafd14cdf6
Amarjit Singh <amarjitmangarhia@gmail.com>
2023-12-05 21:26:56 -0500

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Routes ,Route } from 'react-router-dom';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SigUp';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+       <Routes>
+          <Route path='/services' Component={Services} />
+          <Route path='/products' Component={Products} />
+          <Route path='/sign-up' Component={SignUp} />
+          <Route path='/' Component={Home} />
+       </Routes>
     </div>
   );
 }
+
+         
+
 
 export default App;
